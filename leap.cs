8ce8a086a7d4a00394e0f706b657f9c3092aa9b5
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-04-24 22:34:51 +0100

    Section 4.23

---
+++ leap.cs
@@ -0,0 +1,26 @@
+using System;
+
+class LeapYear
+{
+    static void Main()
+    {
+        //A leap year is a year divisible by 4
+        //BUT a year divisible by 100 is not a leap year unless
+        //it is also divisible by 400
+
+        int year, test1, test2, test3;
+
+        Console.Clear();
+        Console.WriteLine("Please Enter a Year:");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        test1 = year % 4;
+        test2 = year % 100;
+        test3 = year % 400;
+
+        if (test1 == 0 && test2 != 0 || test3 == 0)
+            Console.WriteLine("The year {0} is a leap year.", year);
+        else
+            Console.WriteLine("The year {0} is NOT a leap year.", year);
+    }
+}
