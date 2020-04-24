5645843ea9fa701ff04a51ea48ab28e0ae1c20b1
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-04-24 20:07:17 +0100

    Section 3.15

---
+++ maths.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Maths
+{
+    static void Main()
+    {
+        // F = C * 9/5 + 32
+        float f;
+        float c = 24;
+        f = c * 9/5 +32;
+        
+        Console.WriteLine("{1} Celsius is equal to {0} Fahrenheit", f, c);
+    }
+}
