619701a4df52e6e1777df2e0b02ba61b16874a2f
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-04-24 21:51:29 +0100

    Section 4.21

---
+++ simplecalc.cs
@@ -0,0 +1,40 @@
+using System;
+
+class SimpleCalculator
+{
+    static void Main()
+    {
+        float number1;
+        float number2;
+        string op;
+
+        Console.Clear();
+        Console.WriteLine("Enter Number 1");
+        number1 = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter Number 2");
+        number2 = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter Operator");
+        op = Console.ReadLine();
+
+        switch (op)
+        {
+            case "*":
+                Console.WriteLine("{0}", number1*number2);
+                break;
+            case "/":
+                Console.WriteLine("{0}", number1/number2);
+                break;
+            case "-":
+                Console.WriteLine("{0}", number1-number2);
+                break;
+            case "+":
+                Console.WriteLine("{0}", number1+number2);
+                break;                
+            default:
+                Console.WriteLine("Cannot do calculation");
+                break;
+            
+        }
+
+    }
+}
