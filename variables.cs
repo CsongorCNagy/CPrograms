ede3de5aef225ee9d50c44564f3e2c973c5bda68
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-04-24 15:40:39 +0100

    Section 2.8

---
+++ variables.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Variables
+{
+    static void Main()
+    {
+        int number = 5;
+        int aVal = 7;
+        Console.WriteLine("My value is: {1} and {1}", number, aVal);
+    }
+}
