3bdde08f2bdff572cb5b583859ea48d643f0b275
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-04-25 16:00:30 +0100

    Section 5.26

---
+++ forloop.cs
@@ -0,0 +1,24 @@
+using System;
+
+class TheForLoop
+{
+    static void Main()
+    {
+        int total = 0;
+        float numberOfStudents;
+
+        Console.Clear();
+        Console.WriteLine("Please enter number of students!");
+        numberOfStudents = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Please enter values!");
+
+        for(int i = 1; i <= numberOfStudents; i++)
+        {
+            Console.WriteLine("Enter value {0}", i);
+            total += Convert.ToInt32(Console.ReadLine());
+        }
+
+        float avg = total/numberOfStudents;
+        Console.WriteLine("The Average is {0}", avg);
+    }
+}
