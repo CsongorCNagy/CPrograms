48056d8f3190832d422ef3cab83b5dd4712a33d1
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-04-28 18:08:19 +0100

    Section 8.41

---
+++ CatStrings.cs
@@ -0,0 +1,12 @@
+using System;
+
+class ConcatenateStrings
+{
+    static void Main()
+    {
+        string string1 = "Hello ";
+        string string2 = "There";
+
+        Console.WriteLine(string.Concat(string1,string2));
+    }
+}
