d47328b4c3f18cab81e916c2a0d73a91429a1f8c
Csongor Nagy <csongor.c.nagy@gmail.com>
2020-05-01 16:07:15 +0100

    Section 11.67

--- chatterbot.cs
+++ chatterbot.cs
@@ -5,12 +5,14 @@ using System.IO;
 class ChatBot
 {
     static Dictionary<string,List<string>> kb;
+    static Dictionary<string,List<string>> tp;
     static string[] randomFacts;
     static void Main()
     {
         string input = "";
         randomFacts = File.ReadAllLines("randomfacts.txt");
         LoadKnowledgeFiles(out kb, "knowledge.txt");
+        LoadKnowledgeFiles(out kb, "transpos.txt");
 
         Console.Clear();
         Console.WriteLine("Hello I am Eliza.");
@@ -20,6 +22,7 @@ class ChatBot
             Console.WriteLine("Eliza > " + CalculateResponse(input));
         }
     }
+
     static string CalculateResponse(string input)
     {
         Random r = new Random();
@@ -32,13 +35,37 @@ class ChatBot
         }
 
         List<string> responses = new List<string>();
+
+        if(chance < 40)
+        {
+            string [] words = input.Split(' ');
+            string response = "";
+
+            bool transposed = false;
+            foreach(string w in words)
+            {
+                if(tp.TryGetValue(w, out responses))
+                {
+                    Random rand = new Random();
+                    response += responses[rand.Next(0, responses.Count)] + " ";
+                    transposed = true;
+                }
+                else
+                {
+                    response += w + " ";
+                }
+            }
+            if(transposed) return(response);
+        }
+
+
         if(kb.TryGetValue(input, out responses))
         {
             Random rand = new Random();
             return(responses[rand.Next(0, responses.Count())]);
         }
 
-        //return ("I'm sorry. I don't understand.");
+        return ("I'm sorry. I don't understand.");
     }
 
     static void LoadKnowledgeFiles(out Dictionary<string,List<string>> d, string filename)
